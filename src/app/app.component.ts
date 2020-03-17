b4c8d70480203c955e983c459b8c6eda6f383d54
Joris <guienjoris@gmail.com>
2020-03-17 14:55:55 +0100

    prepare to deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,23 @@ import { ApiService } from './api.service';
 })
 export class AppComponent implements OnInit{
   worldData : any [] = [];
-  confirmedCountry : number;
-  deathCountry: number;
-  recoveredCountry: number;
+  country:string;
+  confirmedCountry : any;
+  deathCountry: any;
+  recoveredCountry: any;
   displayCountry: boolean = false;
   countryName: string ;
   constructor(private api : ApiService){}
   ngOnInit(){
-    this.api.getAll().subscribe((data)=>{
+
+    this.api.getAll().subscribe((data:any)=>{
+
       this.worldData.push(data.latest , data.confirmed.last_updated)
     })
     
   }
   getCountry(country:string){
-    this.api.getAll().subscribe((data)=>{
+    this.api.getAll().subscribe((data:any)=>{
       this.confirmedCountry = data.confirmed.locations.find(e => e.country_code === country) ;
       this.deathCountry = data.deaths.locations.find(e => e.country_code === country);
       this.recoveredCountry = data.recovered.locations.find(e => e.country_code === country)
